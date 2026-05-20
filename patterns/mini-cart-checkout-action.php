9ccb8f5e1b553c49f116388cdb3cdd950f9ed41c
Tung Du <dinhtungdu@gmail.com>
2026-05-20 09:14:49 +0700

    Match mini cart drawer to prototype

--- patterns/mini-cart-checkout-action.php
+++ patterns/mini-cart-checkout-action.php
@@ -7,11 +7,13 @@
 
 $checkout_attrs = wp_json_encode(
 	array(
+		'backgroundColor'      => 'wp',
 		'checkoutButtonLabel' => __( 'proceed to checkout →', 'mercantile-hook-loop' ),
+		'textColor'            => 'card',
 	),
 	JSON_UNESCAPED_UNICODE
 );
 ?>
 <!-- wp:woocommerce/mini-cart-checkout-button-block <?php echo $checkout_attrs; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?> -->
-<div class="wp-block-woocommerce-mini-cart-checkout-button-block"></div>
+<div class="wp-block-woocommerce-mini-cart-checkout-button-block has-card-color has-wp-background-color has-text-color has-background"></div>
 <!-- /wp:woocommerce/mini-cart-checkout-button-block -->
